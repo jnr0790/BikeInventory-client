550ee139d87e45014fe143ae2aa884a93c791dbd
jnr0790 <jromero0790@gmaill.com>
2021-04-25 15:53:18 -0400

    Add heroku link edit

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://bikeinventorydb.herokuapp.com/',
+  production: 'https://bikeinventorydb.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
